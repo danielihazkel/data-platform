2e1aedec7b93ca3fa26f7007bf631815a777106b
danielihazkel <danielihazkel@gmail.com>
2026-02-19 12:05:45 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import Dashboard from './pages/Dashboard';
 import QueriesView from './pages/QueriesView';
 import SchedulesView from './pages/SchedulesView';
 import DistributionsView from './pages/DistributionsView';
+import SettingsView from './pages/SettingsView';
 
 const App: React.FC = () => {
   return (
@@ -15,6 +16,7 @@ const App: React.FC = () => {
           <Route path="/queries" element={<QueriesView />} />
           <Route path="/schedules" element={<SchedulesView />} />
           <Route path="/distributions" element={<DistributionsView />} />
+          <Route path="/settings" element={<SettingsView />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Layout>
